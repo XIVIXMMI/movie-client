8a238d787c027e4825b3e9c7ec46eadd28c08b3c
XIVIXMMI <lethaikhoinguyen@gmail.com>
2023-11-29 19:42:10 +0700

    AddReviews funtion still error

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://4e4a-113-161-43-93.ngrok-free.app',
+    baseURL:'https://2654-2402-800-63ba-8032-a1f0-7cb9-d115-9ee2.ngrok-free.app',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
